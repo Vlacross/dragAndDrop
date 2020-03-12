459590a40d622239543add06053cd3899a452972
Tim Vlasuk <timvlasuk@gmail.com>
2020-03-12 12:24:24 -0700

    added hooks for drag and drop; added links to API docs

--- src/DataList.js
+++ src/DataList.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 
-const Data = ({ data }) => {
+
+const type = "Image";
+
+const Data = ({ data, index }) => {
+  const ref = useRef(null);
+
+  const [ collectedProps, drop ] = useDrop({
+    accept: type,
+    hover(item) {
+      /*syntax of 'hover' from 'useDrop' API:
+        hover(item, monitor) - Optional, called when an item is hovered over the component. You can check 'monitor.isOver({ shallow: true })' to test
+                              whether the hoverhappens over JUST the current target, or over a nested one. Unlike 'drop()', this method will be called
+                              even if 'canDrop()' is defined and returns false (You can check 'monitor.canDrop() to check this case').  
+        src: https://react-dnd.github.io/react-dnd/docs/api/use-drop
+      */
+    }
+  });
+
+  
+  const [{ isDragging }, drag] = useDrag({
+    item: { type, id: data.id, index },
+    collect: monitor => ({
+      /* https://react-dnd.github.io/react-dnd/docs/api/drag-source-monitor */
+      isDragging: monitor.isDragging()
+    })
+  });
+  
+  drag(drop(ref))
+
+
   return (
-    <div className="file-item">
+    <div 
+    ref={ref} 
+    style={{ opacity: isDragging ? 0 : 1 }} 
+    className="file-item"
+    >
       <img alt={`data - ${data.id}`} src={data.src} className="file-data" />
     </div>
   );
